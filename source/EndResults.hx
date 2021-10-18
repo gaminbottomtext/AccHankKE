9017d5d4f970b71076daa49d8af05f8b7e5843d3
tpo <81495861+tposejank@users.noreply.github.com>
2021-10-18 13:52:26 -0500

    things

--- source/EndResults.hx
+++ source/EndResults.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.tweens.FlxTween;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxTimer;
 import flixel.FlxSprite;
 import flixel.FlxG;
@@ -18,6 +20,11 @@ class EndResults extends MusicBeatState
     var fade2:FlxSprite;
     var backdrop:FlxBackdrop;
 
+    var endNumbers:FlxTypedGroup<FlxSprite>;
+    var accAddUp:FlxTypedGroup<FlxSprite>;
+
+    var endNumber:Array<Int> = [0, 0, 0];
+
     var curVal:Int = 0;
 
     var times:Int;
@@ -27,6 +34,10 @@ class EndResults extends MusicBeatState
 
     var bgcolors:Array<Int> = [0xFF5b3c6e, 0xFF5b3c6e, 0xFF5b3c6e, 0xFF3e4599, 0xFF3e4599, 0xFFc16ac8, 0xFF72ccaf];
 
+    var mustFadeAway:Bool = false;
+
+    var percent:FlxSprite;
+
 
     override function create() {
 
@@ -35,9 +46,14 @@ class EndResults extends MusicBeatState
         times = intAccuracy + 1;
         //addScore();
 
+        endNumbers = new FlxTypedGroup<FlxSprite>();
+        accAddUp = new FlxTypedGroup<FlxSprite>();
+        add(endNumbers);
+        add(accAddUp);
+
         backdrop = new FlxBackdrop(Paths.image('menuDesat'));
 		backdrop.y = 0;
-		backdrop.velocity.set(-50, 0);
+		backdrop.velocity.set(50, 0);
 		add(backdrop);
 
         fade = new FlxSprite(0, -415).loadGraphic(Paths.image('ranking/fade'));
@@ -62,6 +78,9 @@ class EndResults extends MusicBeatState
                 }
                 else if (curVal == intAccuracy) {
                     FlxG.sound.play(Paths.sound('confirmMenu'));
+                    mustFadeAway = true;
+                    finish();
+                    popupNumbers();
                 }
 
                 switch (curVal)
@@ -108,21 +127,55 @@ class EndResults extends MusicBeatState
 
         for (i in 0...seperatedScore.length)
             {
-                var numScore:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('num' + Std.int(seperatedScore[i])));
+                var numScore:FlxSprite = new FlxSprite(800, 75).loadGraphic(Paths.image('num' + Std.int(seperatedScore[i])));
                 remove(numScore);
-                numScore.x = 0 + (i * 100);
-                add(numScore);
+                numScore.x = 800 + (i * 100);
+                accAddUp.add(numScore);
+                if (!mustFadeAway) {
+                    add(numScore);
+                }
+                else {
+                    accAddUp.forEach(function(spr:FlxSprite) {
+                        FlxTween.tween(spr, {alpha: 0}, 1);
+                    });
+                }
             }
+        
+        endNumber[0] = seperatedScore[0];
+        endNumber[1] = seperatedScore[1];
+        endNumber[2] = seperatedScore[2];
     }
 
     override function update(elapsed:Float) {
         updateRankLetter();
     }
 
+    
+
     function updateRankLetter() {
         backdrop.color = bgcolors[rankInt];
         remove(rankLetter);
-        rankLetter = new FlxSprite(0, 100).loadGraphic(Paths.image('ranking/' + ranks[rankInt]));
+        rankLetter = new FlxSprite(850, 230).loadGraphic(Paths.image('ranking/' + ranks[rankInt]));
         add(rankLetter);
+    }
+
+    function finish() {
+        for (i in 0...endNumber.length)
+            {
+                var number:FlxSprite = new FlxSprite(410, 310).loadGraphic(Paths.image('num' + endNumber[i]));
+                number.x = 410 + (i * 100);
+                endNumbers.add(number);
+                number.alpha = 0;
+                add(number);
+            }
+
+        endNumbers.forEach(function(spr:FlxSprite) {
+            FlxTween.tween(spr, {alpha: 1}, 1);
+        });
+
+        percent = new FlxSprite(740, 360).loadGraphic(Paths.image('ranking/percent'));
+        percent.scale.set(2, 2);
+        percent.antialiasing = true;
+        add(percent);
     }
 }
