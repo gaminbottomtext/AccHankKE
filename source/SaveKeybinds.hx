2c34b122a714d326ad31557f459ed898b45dae38
tpo <81495861+tposejank@users.noreply.github.com>
2021-10-29 08:46:49 -0500

    ting

--- source/SaveKeybinds.hx
+++ source/SaveKeybinds.hx
@@ -8,7 +8,7 @@ class SaveKeybinds extends MusicBeatState
 {
     var holdTimer:Float = 0;
     override function create() {
-        var warning:FlxText = new FlxText(0, 0, 0, 'Please make sure to edit your keybinds. \n\nHold Space to continue.', 42);
+        var warning:FlxText = new FlxText(0, 0, FlxG.width, 'Make sure to edit your keybinds. Hold space to continue.', 42);
         warning.setFormat(Paths.font("vcr.ttf"), 42, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
         warning.screenCenter();
         add(warning);
@@ -19,6 +19,7 @@ class SaveKeybinds extends MusicBeatState
 
         if (FlxG.keys.pressed.SPACE) {
             holdTimer += elapsed;
+            trace(holdTimer);
         }
         if (FlxG.keys.justReleased.SPACE) {
             holdTimer = 0;
